c52bd04d329e6dfba84c327465638f12400eb26b
ncuwym <ncuwym@qq.com>
2020-09-20 23:36:43 +0800

    springboot基础框架

---
+++ module/src/test/java/cn/ncu/wym/leisure/LeisureApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.ncu.wym.leisure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeisureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
